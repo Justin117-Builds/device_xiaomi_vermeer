d2349f32f145fa921ad0a9ca97fae576bf8a45c3
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-10-23 19:38:53 -0500

    vermeer: Use my device repo for POCO F6 Pro / Redmi K70

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,25 @@
 # Vendor  
-git clone --depth=1 https://github.com/Spacemk1014/proprietary_vendor_xiaomi.git vendor/xiaomi 
+git clone --depth=1 https://github.com/Justin117-Builds/vendor_xiaomi.git vendor/xiaomi 
 
 # Kernel
 rm -rf device/xiaomi/vermeer-kernel 
-git clone --depth=1 https://github.com/Spacemk1014/android_device_xiaomi_vermeer-kernel.git device/xiaomi/vermeer-kernel 
+git clone --depth=1 https://github.com/Justin117-Builds/device_xiaomi_vermeer-kernel.git device/xiaomi/vermeer-kernel 
 
 # Camera 
 git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm8550 vendor/xiaomi/camera 
 
 # Hardware 
-git clone --depth=1 https://github.com/Spacemk1014/android_hardware_xiaomi.git hardware/xiaomi 
+git clone --depth=1 https://github.com/Justin117-Builds/hardware_xiaomi.git hardware/xiaomi 
 
 # Sepolicy 
-git clone --depth=1 https://github.com/Spacemk1014/android_device_xiaomi_sepolicy.git device/xiaomi/sepolicy 
+git clone --depth=1 https://github.com/Justin117-Builds/android_device_xiaomi_sepolicy.git device/xiaomi/sepolicy 
 
 # Hardware Qcom
-git clone --depth=1 https://github.com/Spacemk1014/android_hardware_qcom-caf_sm8550_gps.git hardware/qcom-caf/sm8550/gps
+git clone --depth=1 https://github.com/Justin117-Buidls/android_hardware_qcom-caf_sm8550_gps.git hardware/qcom-caf/sm8550/gps
 
 # Sepolicy_vndr
 rm -rf device/qcom/sepolicy_vndr/sm8550
-git clone --depth=1 https://github.com/Spacemk1014/android_device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8550
+git clone --depth=1 https://github.com/Justin117-Builds/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8550
 
 # System Qcom
-git clone --depth=1 https://github.com/Spacemk1014/android_system_qcom.git system/qcom
+git clone --depth=1 https://github.com/Justin117-Builds/android_system_qcom.git system/qcom
